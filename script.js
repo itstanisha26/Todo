2b20a5c73b553e495cabdc3379065e55e02203b6
Tanisha Sharma <itstanisha@gmail.com>
2022-06-01 15:25:02 +0530

    icon functionality

--- script.js
+++ script.js
@@ -2,48 +2,52 @@
 
 const inputEl = document.querySelector('.todo__input');
 const todoContainer = document.querySelector('.todo__container');
-const deleteIcon = document.querySelector('.delete-icon');
 const errorMssg = document.querySelector('.error');
-const iconDone = document.querySelector('.icon-container--done');
-
-let todoArray = [];
+const textEncourage = document.querySelector('.todo__text--encourage');
+const todoTaskContainer = document.querySelector('.todo__task-container');
 
 inputEl.addEventListener('keydown', (e) => {
-  errorMssg.style.display = 'none';
-
   let inputValue = inputEl.value;
+  errorMssg.style.display = 'none';
+  textEncourage.style.display = 'none';
 
-  if (inputValue !== '' && e.key === 'Enter') {
-    const html = `
-    <div class="todo__task-container">
-          <span class="todo__text"> ${inputValue}</span>
+  if (inputValue && e.key === 'Enter') {
+    const todo = document.createElement('p');
 
-          <div class="icons-container">
-            <div class="icon-container icon-container--done">
-              <ion-icon name="checkmark-outline" class="icon done-icon">
-              </ion-icon>
-            </div>
-            <div class="icon-container icon-container--delete">
-              <ion-icon
-                name="trash-outline"
-                class="icon delete-icon"
-              ></ion-icon>
-            </div>
-          </div>
-        </div>
-    `;
+    todo.innerHTML = `
+    <div class='todo__task'>
+       <span class='todo__text'> ${inputValue}</span>
+       <div class='icons-container'>
+         <div class='icon-container icon-container--done'>
+           <ion-icon name='checkmark-outline' class='icon done-icon'></ion-icon>
+         </div>
+         <div class='icon-container icon-container--delete'>
+           <ion-icon name='trash-outline' class='icon delete-icon'></ion-icon>
+         </div>
+       </div>
+     </div>
+`;
+    todoTaskContainer.append(todo);
 
-    todoContainer.insertAdjacentHTML('beforeend', html);
-    inputEl.value = '';
-    inputEl.placeholder = 'Not enough! Add another one';
+    // Implementing icon functionality
+    const deleteIcon = document.querySelectorAll('.delete-icon');
+    const iconDone = document.querySelectorAll('.icon-container--done');
+
+    const workTodo = (i) => {
+      i.addEventListener('click', (e) => {
+        const parent = e.target.closest('.todo__task');
+        i.classList.contains('delete-icon')
+          ? parent.remove()
+          : (parent.style.opacity = 0.4);
+      });
+    };
+
+    deleteIcon.forEach((i) => workTodo(i));
+    iconDone.forEach((i) => workTodo(i));
   }
 
-  //
-  else if (inputValue === '' && e.key === 'Enter')
-    errorMssg.style.display = 'block';
-});
+  // If input is empty
+  else if (e.key === 'Enter') errorMssg.style.display = 'block';
 
-// PROBLEMS
-// 1) not adding empty string to todo
-// 2) not adding already included item
-//
+  inputEl.value = '';
+});
